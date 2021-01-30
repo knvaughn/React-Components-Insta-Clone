6a9819b75b1a5da8e8024aa6468c9647dd106ee8
Kristin Vaughn <vaughn.kristin.n@gmail.com>
2021-01-29 19:36:43 -0500

    Create Instagram Clone

--- src/components/Posts/LikeSection.js
+++ src/components/Posts/LikeSection.js
@@ -5,10 +5,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faHeart } from '@fortawesome/free-regular-svg-icons';
+import { faHeart as fasHeart } from '@fortawesome/free-solid-svg-icons';
 
 const LikeSection = props => {
   // 🔥 Make sure the parent of LikeSection is passing the right props!
-  const { likePost, numberOfLikes } = props;
+  const { likePost, numberOfLikes, isLiked } = props;
 
   return (
     <div>
@@ -17,13 +18,13 @@ const LikeSection = props => {
         key='likes-icons-container'
       >
         <div className='like-section-wrapper'>
-          <FontAwesomeIcon icon={faHeart} />
+          <FontAwesomeIcon icon={isLiked ? fasHeart : faHeart} onClick={likePost} />
         </div>
         <div className='like-section-wrapper'>
           <FontAwesomeIcon icon={faComment} />
         </div>
       </div>
-      <p className='like-number'>100 likes</p>
+      <p className='like-number'>{numberOfLikes} likes</p>
     </div>
   );
 };
--- src/dummy-data.js
+++ src/dummy-data.js
@@ -54,6 +54,36 @@ const dummyData = [
             },
         ],
     },
+    {
+        id: 3,
+        username: "lolcats",
+        thumbnailUrl: 'https://icon2.cleanpng.com/20180320/sqe/kisspng-twitch-computer-icons-streaming-media-youtube-live-tv-twitch-icon-5ab19172461392.001176751521586546287.jpg',
+        imageUrl: 'https://images.unsplash.com/photo-1491485880348-85d48a9e5312?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+        likes: 9001,
+        timestamp: "July 15th 2017, 03:12:09 pm",
+        comments: [
+            {
+                id: 36,
+                username: "fluffythecat",
+                text: "Meow",
+            },
+            {
+                id: 37,
+                username: "mina97",
+                text: "=^___^=",
+            },
+            {
+                id: 38,
+                username: "cat_fanatic",
+                text: "Cats rule",
+            },
+            {
+                id: 39,
+                username: "catastic",
+                text: "I am a cat",
+            },
+        ],
+    }
 ];
 
 export default dummyData;
